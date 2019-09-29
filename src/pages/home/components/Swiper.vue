0703f8cea1672188c8c2a3bfa7a2a087f1541ad6
MaoDouDou <670890875@qq.com>
2019-09-29 14:49:49 +0800

    首页轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" />
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {id: '001', imgUrl: 'static/imgs/01.jpg'},
+        {id: '002', imgUrl: 'static/imgs/02.jpg'}
+      ]
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+      background: #ffffff !important
+    .wrapper
+      overflow: hidden
+      width: 100%
+      height: 0
+      padding-bottom: 26.66%
+      background: #eee
+      .swiper-img
+          width: 100%
+</style>
